a877a9e1e4a1576d44670e878404bdd50e1d2eb7
Ygor da Rosa <yygorrosa@gmail.com>
2024-10-04 23:01:48 -0300

    feat: projeto iniciado

---
+++ runs-por-nivel.py
@@ -0,0 +1,52 @@
+import discord
+from utils import niveis
+import math
+import os
+
+
+
+# Criar a instância do cliente Discord
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+TOKEN = os.environ["bot-key"]
+
+@client.event
+async def on_ready():
+    print(f'Bot {client.user} está pronto!')
+
+# Função para executar quando uma mensagem for enviada
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    # Comando para calcular o XP do Digimon
+    if message.content.startswith('!calcular'):
+        try:
+            # Extrair o nível do comando
+            nivel = message.content.split()[1]
+
+            # Validar se o nível é um número e se está no intervalo dos níveis
+            if nivel in niveis:
+                xp_por_ch = 14947386124
+                nivel_xp = niveis[nivel]
+                ch_por_nivel = math.ceil(nivel_xp / xp_por_ch)
+                
+                # Responder no chat com o resultado
+                await message.channel.send(f"Você precisa fazer {ch_por_nivel} CH's da run para upar pro nível {int(nivel) + 1}.")
+                await message.channel.send(f"Os cálculos foram feitos tendo como base 4250% de XP contando com o 500% padrão.")
+            else:
+                await message.channel.send("Digite um nível entre 150 e 170, ou a informação ainda não está disponível.")
+        
+        except (IndexError, ValueError):
+            await message.channel.send("Por favor, forneça um número válido de nível após o comando. Exemplo: `!calcular 150`")
+
+# Rodar o bot
+client.run(TOKEN)
+
+
+
+
+
